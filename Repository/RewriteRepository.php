595e4ea6aac9ef6c0ad3fa2f1e9a90f7d06982e3
Nicolas Renault <nrenault@tangkoko.com>
2022-09-08 12:58:03 +0200

    fix php8.1 compatibulity

--- Repository/RewriteRepository.php
+++ Repository/RewriteRepository.php
@@ -23,8 +23,7 @@ class RewriteRepository implements RewriteRepositoryInterface
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \MageSuite\UrlRewrite\Model\RewriteFactory $rewriteFactory
-    )
-    {
+    ) {
         $this->scopeConfig = $scopeConfig;
         $this->rewriteFactory = $rewriteFactory;
     }
@@ -32,41 +31,45 @@ class RewriteRepository implements RewriteRepositoryInterface
     /**
      * @inheritdoc
      */
-    public function getRewrite($requestUri) {
+    public function getRewrite($requestUri)
+    {
         $rewrites = $this->scopeConfig->getValue('web/custom_rewrites/rewrites', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $rewrites = $this->cleanRewritesList($rewrites);
-        $rewrites = explode(PHP_EOL, $rewrites);
+        if ($rewrites) {
+            $rewrites = $this->cleanRewritesList($rewrites);
+            $rewrites = explode(PHP_EOL, $rewrites);
 
-        foreach($rewrites as $rewrite) {
-            $rewrite = explode('|', $rewrite);
+            foreach ($rewrites as $rewrite) {
+                $rewrite = explode('|', $rewrite);
 
-            if(!isset($rewrite[self::ORIGINAL_URL]) or !isset($rewrite[self::TARGET_URL])) {
-                continue;
+                if (!isset($rewrite[self::ORIGINAL_URL]) or !isset($rewrite[self::TARGET_URL])) {
+                    continue;
+                }
+
+                $originalUrl = ltrim($rewrite[self::ORIGINAL_URL], '/');
+                $originalUrl = trim($originalUrl);
+
+                if ($originalUrl != $requestUri) {
+                    continue;
+                }
+
+                /** @var \MageSuite\UrlRewrite\Model\Rewrite $rewrite */
+                $targetRewrite = $this->rewriteFactory->create();
+
+                $targetRewrite->setTargetUrl($rewrite[self::TARGET_URL]);
+                $targetRewrite->setStatusCode($rewrite[self::STATUS_CODE] ?? self::DEFAULT_STATUS_CODE);
+
+                return $targetRewrite;
             }
-
-            $originalUrl = ltrim($rewrite[self::ORIGINAL_URL], '/');
-            $originalUrl = trim($originalUrl);
-
-            if($originalUrl != $requestUri) {
-                continue;
-            }
-
-            /** @var \MageSuite\UrlRewrite\Model\Rewrite $rewrite */
-            $targetRewrite = $this->rewriteFactory->create();
-
-            $targetRewrite->setTargetUrl($rewrite[self::TARGET_URL]);
-            $targetRewrite->setStatusCode($rewrite[self::STATUS_CODE] ?? self::DEFAULT_STATUS_CODE);
-
-            return $targetRewrite;
         }
-
         return null;
     }
 
-    public function cleanRewritesList($rewritesList) {
+    public function cleanRewritesList($rewritesList)
+    {
         $rewritesList = str_replace("\r\n", "\n", $rewritesList);
         $rewritesList = str_replace("\r", "\n", $rewritesList);
 
         return $rewritesList;
     }
 }
+
